0b7b78b2047b83f811fc9f6d6af34e71c4f6c672
gj <gemtab@gmail.com>
2024-03-25 21:27:01 +0000

    updating

--- CCSC.py
+++ CCSC.py
@@ -1,4 +1,8 @@
-import simpy, pandas, matplotlib
+import simpy
+import pandas
+import matplotlib
 
-print("hello")
-print("but why?")
+#developing the ability to model the function of the colon cancer screening centre
+
+#To do:
+#Develop the functions to: assign probability, define each resource, define each outcome, determine what I want to measure and report

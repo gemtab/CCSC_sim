317ef7f11fc33bc977d2917f8646c7a0fd85e145
gj <gemtab@gmail.com>
2024-03-29 17:31:27 +0000

    working on random number gen

--- CCSC.py
+++ CCSC.py
@@ -1,9 +1,16 @@
 import simpy
-import pandas
+import pandas 
 import matplotlib
-
+from random import choices
 #developing the ability to model the function of the colon cancer screening centre
 
 #To do:
-#Develop the functions to: assign probability, define each resource, define each outcome, determine what I want to measure and report
-print("hello world")
+#Develop the functions to: assign probability, define each resource, define each outcome, determine what I want to 
+# measure and report
+
+def time_multiplier(bottom_range, top_range):
+    times = list(range(bottom_range, top_range + 1))
+    return(times)
+
+x= time_multiplier(1,5)
+print(x)
